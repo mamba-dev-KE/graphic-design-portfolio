36736f0b76e404ed3a60726939b877c3103f80d6
Joseph Maramba <josephmaramba50@gmail.com>
2022-02-02 07:22:41 +0300

    moving on to projects

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
 					src="https://images.unsplash.com/photo-1622299701772-dcad96233a79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
 					alt=""
 				/>
-				<div>
+				<div className={styles.intro}>
 					<h1 className={styles.hello}>Hi, I'm Dennis</h1>
 					<p className={styles.about_text}>
 						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
