27206733127187ef02a5f4b84caa61feb40a938b
Joseph Maramba <josephmaramba50@gmail.com>
2022-02-02 07:12:29 +0300

    about section progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 import "./index.css";
 
 const App = () => {
 	return (
 		<div>
 			<Header />
-			<Hero />
+			<About />
 		</div>
 	);
 };
---
+++ src/components/About/About.jsx
@@ -0,0 +1,28 @@
+import styles from "./about.module.css";
+
+const About = () => {
+	return (
+		<section className={styles.about}>
+			<div className={styles.row}>
+				<img
+					className={styles.about_img}
+					src="https://images.unsplash.com/photo-1622299701772-dcad96233a79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+					alt=""
+				/>
+				<div>
+					<h1 className={styles.hello}>Hi, I'm Dennis</h1>
+					<p className={styles.about_text}>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+						quisquam qui reprehenderit dolorem facilis ad voluptate amet
+						asperiores accusantium tempora quae facere enim ipsum quos non,
+						expedita recusandae harum, rerum nobis quod sint nesciunt natus
+						aliquid officia? Nostrum eligendi, soluta harum architecto rem
+						veritatis iste corrupti facere modi, sint quibusdam!
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default About;
