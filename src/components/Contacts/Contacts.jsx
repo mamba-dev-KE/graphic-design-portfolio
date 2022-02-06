268247471612877b7d5084dc17b2e315638de7ee
Joseph Maramba <josephmaramba50@gmail.com>
2022-02-06 10:26:51 +0300

    interesting look

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -5,6 +5,18 @@ const Contacts = () => {
 		<section className={styles.contacts}>
 			<div className={styles.container}>
 				<h2 className={styles.contacts_title}>Contacts</h2>
+				<div className={styles.row}>
+					<div className={styles.contacts_text}>
+						<h3>Want to transform your vision into an A++ design??</h3>
+						<p>Contact me!</p>
+					</div>
+					<div className={styles.contacts_img}>
+						<img
+							src="https://blush.design/api/download?shareUri=AzMoqXcn-NQYOXyX&c=Skin_0%7E1fad66&w=800&h=800&fm=png"
+							alt=""
+						/>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
